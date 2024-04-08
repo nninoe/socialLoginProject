926ccec1e144ff1c2a07b447d0e3622be9cef2d4
unknown <2023086799@hanyang.ac.kr>
2024-04-08 18:58:02 +0900

    hi

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
 
+import { ping } from './controller/system.js';
+
 const serverPort = 3000;
 const app = express();
 app.use(bodyParser.json());
@@ -9,13 +11,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 const router = express.Router();
 
-const ping = (req, res) => {
-    const result = true;
-    res.send({result});
-};
-
 router.route('/ping').get(ping);
 
 app.use('/socialLoginProject/api/v1',router);
 app.listen(serverPort);
-//http://localhost:3000/socialLoginProject/api/v1/ping
+//http://13.49.21.109:3000/socialLoginProject/api/v1/ping
